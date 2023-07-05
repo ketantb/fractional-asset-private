856a966b850c54be1e801f859d2e0e798e61fc7d
Yash Bele <yashgbele@gmail.com>
2023-07-05 17:30:19 +0530

    make changes asked by Harshada

--- client/src/components/my-profile-section/table-row-to-show-property-details-in-table/SellRow.js
+++ client/src/components/my-profile-section/table-row-to-show-property-details-in-table/SellRow.js
@@ -4,22 +4,24 @@ import { RiDeleteBin6Fill } from "react-icons/ri";
 import { AiTwotoneEdit } from "react-icons/ai";
 import { Modal } from "react-responsive-modal";
 import "react-responsive-modal/styles.css";
-import { Link } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import axios from "../../../helpers/axios";
+import { useNavigate } from "react-router-dom";
+import { TiTick } from "react-icons/ti";
+import { AiOutlineWarning } from "react-icons/ai";
 
 const SellRow = ({ data, getData }) => {
+  const navigate = useNavigate();
   // handle modal
-  const [open, setOpen] = useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
   const handleDeleteOpenModal = () => {
-    console.log(open);
-    setOpen(true);
+    setOpenDeleteModal(true);
   };
   const handleDeleteCloseModal = () => {
-    setOpen(false);
+    setOpenDeleteModal(false);
   };
 
-  //handle Yes
+  //handle Yes for delete
   const handleYes = async (id) => {
     console.log(id);
     toast.loading("Deleting data...");
@@ -34,8 +36,36 @@ const SellRow = ({ data, getData }) => {
       console.log(err);
     }
   };
+
+  //handle edit
+  const handleEdit = (id, type) => {
+    console.log(id);
+    if (type === "apartment") {
+      navigate(`/apartment-edit/${id}`);
+    } else if (type === "resort") {
+      navigate(`/resort-edit/${id}`);
+    } else if (type === "car") {
+      navigate(`/car-edit/${id}`);
+    } else if (type === "villa") {
+      navigate(`/villa-edit/${id}`);
+    } else if (type === "shop") {
+      navigate(`/shop-edit/${id}`);
+    }
+  };
+
   return (
     <tr className="sell-row">
+      {data.isVerified ? (
+        <th data-label="status">
+          <TiTick style={{ color: "green" }} />
+          <span style={{ color: "green" }}>Verified</span>
+        </th>
+      ) : (
+        <th data-label="status">
+          <AiOutlineWarning style={{ color: "#E5A805" }} />
+          <span style={{ color: "#E5A805" }}>Under screening...</span>
+        </th>
+      )}
       <td data-label="Posted-on">{data.postedOn}</td>
       <td data-label="Type">{data.propertyType}</td>
       <td data-label="Total Shares">{data.totalShares}</td>
@@ -53,12 +83,14 @@ const SellRow = ({ data, getData }) => {
           />
         </div>
         <div>
-          <AiTwotoneEdit className="view-edit-icon" />
+          <AiTwotoneEdit
+            className="view-edit-icon"
+            onClick={() => handleEdit(data._id, data.propertyType)}
+          />
         </div>
       </td>
-
       <>
-        <Modal open={open} onClose={handleDeleteCloseModal} center>
+        <Modal open={openDeleteModal} onClose={handleDeleteCloseModal} center>
           <div className="deleteModalWrap">
             <h6>Are you sure want to delete this data?</h6>
             <div className="buttons-wrap">
