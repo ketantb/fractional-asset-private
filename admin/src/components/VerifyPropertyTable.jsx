8e412894bb6308cc41b0cf76f29beb0778a16148
Yash Bele <yashgbele@gmail.com>
2023-07-06 17:31:57 +0530

    correct table alignment in all tables - admin side

--- admin/src/components/VerifyPropertyTable.jsx
+++ admin/src/components/VerifyPropertyTable.jsx
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 const Table = ({ data }) => {
   return (
     <main className="absolute w-full h-full shadow-md font-sans">
-      <table className="w-[100%] text-xs text-left text-gray-400 mx-auto">
-        <thead className="text-sm uppercase bg-gray-700 text-gray-400 sticky top-0 font-serif">
+      <table className="w-[90%] text-xs text-left text-gray-400 mx-auto">
+        <thead className="text-xs uppercase bg-gray-700 text-gray-400 sticky top-0 font-serif">
           <tr>
-            <th scope="col" className="w-[20%] pl-9 py-3">
+            <th scope="col" className="pl-9 py-3">
               Unique Id
             </th>
-            <th scope="col" className="w-[20%] pl-9 py-3">
+            <th scope="col" className="py-3">
               Posted On
             </th>
-            <th scope="col" className="w-[20%] pl-9 py-3">
+            <th scope="col" className="py-3">
               Seller Type
             </th>
-            <th scope="col" className="w-[20%] pl-9 py-3">
+            <th scope="col" className="py-3">
               Seller Name
             </th>
-            <th scope="col" className="w-[20%] py-3 ">
+            <th scope="col" className="py-3 ">
               Property Type
             </th>
-            <th scope="col" className="w-[20%] py-3">
+            <th scope="col" className=" py-3 text-center">
               Action
             </th>
           </tr>
@@ -30,28 +30,28 @@ const Table = ({ data }) => {
           {data.map((i, j) => (
             <tr
               key={i._id}
-              className={`border-b border-gray-700 capitalize ${
+              className={`border-b border-gray-700 capitalize font-montserrat ${
                 j % 2 === 0 ? "bg-gray-900" : "bg-gray-800"
               }`}
             >
               <th
                 scope="row"
-                className={`text-sm pl-9 font-normal text-gray-400 ${
+                className={`text-xs pl-9 font-normal text-gray-400 ${
                   j % 2 === 0 ? "py-3" : "py-2.5"
                 }`}
               >
                 {i.uniqueId || "---"}
               </th>
-              <td className={`text-sm ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
+              <td className={`text-xs ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
                 {i.postedOn || "---"}
               </td>
-              <td className={`text-sm ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
+              <td className={`text-xs ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
                 {i.sellerType || "---"}
               </td>
-              <td className={`text-sm ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
+              <td className={`text-xs ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
                 {i.sellerName || "---"}
               </td>
-              <td className={`text-sm ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
+              <td className={`text-xs ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
                 {i.propertyType || "---"}
               </td>
               <td className={`text-center ${j % 2 === 0 ? "py-3" : "py-2.5"}`}>
