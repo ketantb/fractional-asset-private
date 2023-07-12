0ef401a79ae41634467d71d4c124cd4e51895af8
Harshada0611 <harshadaa1997@gmail.com>
2023-07-12 18:03:50 +0530

    navbar

--- client/src/components/app-bar/MobileNav.js
+++ client/src/components/app-bar/MobileNav.js
@@ -1,56 +1,255 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
-export default function MobileNav({ CiMenuBurger }) {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
+const MobileNav = ({
+  auth,
+  setAuth,
+  realEstateArr,
+  otherCategoryArr,
+  collectionNames,
+}) => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    setAuth(token);
+    console.log(collectionNames);
+    //eslint-disable-next-line
+  }, [token]);
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
+  // FORM DROPDOWN
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showNestedDropdown, setShowNestedDropdown] = useState(false);
+  const handleNonRealEstateForms = (name) => {
+    navigate(`${name}-form`); // Replace "/next-page" with the actual route you want to navigate to
+    setShowDropdown(false);
   };
+  const handleRealEstateForm = (name) => {
+    // navigate(`/nested-next-page/${option}`); // Replace "/nested-next-page" with the actual route for the nested dropdown option
+    navigate(`${name}-form`); // Replace "/nested-next-page" with the actual route for the nested dropdown option
+    setShowDropdown(false);
+    setShowNestedDropdown(false);
+  };
+  //FORM DROPDOWN ENDS
 
-  const list = (anchor) => (
-    <Box
-      sx={{ width: anchor === "bottom" ? "auto" : 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>Real estate list</List>
-      <Divider />
-      <List></List>
-    </Box>
-  );
+  //LANDING PAGE DROPDOWN
+  const [showLandingDropdown, setShowLandingDropdown] = useState(false);
+  const [showNestedLandingDropdown, setShowNestedLandingDropdown] =
+    useState(false);
+  const naviagteToRealEstatePages = (name) => {
+    navigate(`${name}-page`); // Replace "/nested-next-page" with the actual route for the nested dropdown option
+    setShowNestedLandingDropdown(false);
+  };
+  const naviagteToNonRealEstatePages = (name) => {
+    navigate(`${name}-page`); // Replace "/next-page" with the actual route you want to navigate to
+    setShowLandingDropdown(false);
+  };
+  // LANDING PAGE DROPDOWN ENDS
+
+  //close post property form dropdown on clicking anywhere window
+  const dropdownRef = useRef(null);
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        // form dropdown
+        setShowDropdown(false);
+        setShowNestedDropdown(false);
+
+        // landing page dropdown
+        // setShowLandingDropdown(false);
+        // setShowNestedLandingDropdown(false);
+      }
+    };
+    window.addEventListener("click", handleOutsideClick);
+    return () => {
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+  //close post property dropdown on clicking anywhere window ends
+
+  //close post property form dropdown on clicking anywhere window
+  const landingDropdownRef = useRef(null);
+  useEffect(() => {
+    const handleOutsideClickForLandingDropdown = (event) => {
+      if (
+        landingDropdownRef.current &&
+        !landingDropdownRef.current.contains(event.target)
+      ) {
+        setShowLandingDropdown(false);
+        setShowNestedLandingDropdown(false);
+      }
+    };
+    window.addEventListener("click", handleOutsideClickForLandingDropdown);
+    return () => {
+      window.removeEventListener("click", handleOutsideClickForLandingDropdown);
+    };
+  }, []);
+  //close post property dropdown on clicking anywhere window ends
 
   return (
-    <div>
-      {["left"].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}></Button>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
+    <div className="app-navbar-wrap">
+      {/* LANDING PAGE DROPDOWN */}
+      <div className="col1" ref={landingDropdownRef}>
+        <Button
+          onClick={() => setShowLandingDropdown(!showLandingDropdown)}
+          variant="outlined"
+          endIcon={<MdKeyboardArrowDown />}
+        >
+          Fractional Properties
+        </Button>
+        {showLandingDropdown && (
+          <div className="hidden-dropdown">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() =>
+                    setShowNestedLandingDropdown(!showNestedLandingDropdown)
+                  }
+                >
+                  <ListItemText primary="Real Estate" />
+                  <ListItemIcon>
+                    <MdKeyboardArrowRight className="text-white" />
+                  </ListItemIcon>
+                </ListItemButton>
+              </ListItem>
+              {showNestedLandingDropdown && (
+                <div className="hidden-nested-dropdown">
+                  <List>
+                    {realEstateArr.map((name, i) => (
+                      <ListItem disablePadding>
+                        <ListItemButton
+                          onClick={() => naviagteToRealEstatePages(name)}
+                        >
+                          <ListItemText primary={name} />
+                        </ListItemButton>
+                      </ListItem>
+                    ))}
+                  </List>
+                </div>
+              )}
+              {/* {otherCategoryArr.map((name, i) => (
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => naviagteToNonRealEstatePages(name)}
+                  >
+                    <ListItemText primary={name} />
+                  </ListItemButton>
+                </ListItem>
+              ))} */}
+            </List>
+          </div>
+        )}
+      </div>
+      {/* LANDING PAGE DROPDOWN ENDS */}
+      <div className="col2" ref={dropdownRef}>
+        <div className="subcol2-5 navbar-item">
+          <Button
+            onClick={() => setShowDropdown(!showDropdown)}
+            variant="outlined"
+            endIcon={<MdKeyboardArrowDown />}
           >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+            List Property
+          </Button>
+          {showDropdown && (
+            <div className="hidden-dropdown-2">
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={() => setShowNestedDropdown(!showNestedDropdown)}
+                  >
+                    <ListItemText primary="Real Estate" />
+                    <ListItemIcon>
+                      <MdKeyboardArrowRight className="text-white" />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+                {showNestedDropdown && (
+                  <div className="hidden-nested-dropdown-2">
+                    <List>
+                      {realEstateArr.map((name, i) => (
+                        <ListItem disablePadding>
+                          <ListItemButton
+                            onClick={() => handleRealEstateForm(name)}
+                          >
+                            <ListItemText primary={name} />
+                          </ListItemButton>
+                        </ListItem>
+                      ))}
+                    </List>
+                  </div>
+                )}
+                {/* {otherCategoryArr.map((name, i) => (
+                  <ListItem disablePadding>
+                    <ListItemButton
+                      onClick={() => handleNonRealEstateForms(name)}
+                    >
+                      <ListItemText primary={name} />
+                    </ListItemButton>
+                  </ListItem>
+                ))} */}
+              </List>
+            </div>
+          )}
+        </div>
+        <div className="subcol2-1 navbar-item" onClick={() => navigate("/")}>
+          Home
+        </div>
+        <div
+          className="subcol2-2 navbar-item"
+          onClick={() => navigate("/how-it-works")}
+        >
+          How It Works
+        </div>
+        <div
+          className="subcol2-3 navbar-item"
+          onClick={() => navigate("/aboutus")}
+        >
+          About Us
+        </div>
+        <div
+          className="subcol2-3 navbar-item"
+          onClick={() => navigate("/channel-partner")}
+        >
+          Channel Partner
+        </div>
+        {auth ? (
+          <div
+            className="subcol2-6 navbar-item"
+            onClick={() => navigate("/my-profile")}
+          >
+            My Profile
+          </div>
+        ) : null}
+        {auth ? (
+          <div
+            className="subcol2-7 navbar-item"
+            onClick={() => {
+              localStorage.clear();
+              navigate("/");
+            }}
+          >
+            Logout
+          </div>
+        ) : (
+          <div
+            className="subcol2-7 navbar-item"
+            onClick={() => navigate("/signin")}
+          >
+            Login
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default MobileNav;
