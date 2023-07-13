1f463b4396d1b8d6ae9120d73cd6c273167dc5a7
Yash Bele <yashgbele@gmail.com>
2023-07-13 10:11:42 +0530

    make home page vertical slider

--- client/src/components/testimonial/testimonial.js
+++ client/src/components/testimonial/testimonial.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react';
-import './testimonial.css'
-import fractionalOwnershipExplained from '../../assets/Fractional Ownership Explained -.mp4'
+import React, { useState } from "react";
+import "./testimonial.css";
+import fractionalOwnershipExplained from "../../assets/Fractional Ownership Explained -.mp4";
 
 const TestimonialSlider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  
+
   const testimonials = [
     {
       id: 1,
-      videoUrl: 'https://www.youtube.com/watch?v=4Js1EYLd2vg',
+      videoUrl: "https://www.youtube.com/watch?v=4Js1EYLd2vg",
     },
     {
       id: 2,
-      videoUrl: 'https://www.youtube.com/watch?v=4Js1EYLd2vg',
+      videoUrl: "https://www.youtube.com/watch?v=4Js1EYLd2vg",
     },
     {
       id: 3,
-      videoUrl: 'https://www.youtube.com/watch?v=4Js1EYLd2vg',
+      videoUrl: "https://www.youtube.com/watch?v=4Js1EYLd2vg",
     },
   ];
-  
+
   const handleSlideChange = (index) => {
     setActiveIndex(index);
-  }
-  
+  };
+
   return (
     <div className="testimonial-slider">
-        <h2>Food For Thoughts</h2>
+      <h2>Food For Thoughts</h2>
       <div className="video-container">
         {/* <video src={testimonials[activeIndex].videoUrl} controls autoPlay loop /> */}
-        <video src={fractionalOwnershipExplained} controls loop muted />
+        <video src={fractionalOwnershipExplained} autoPlay loop muted />
       </div>
-      <div className="slider-nav">
+      {/* <div className="slider-nav">
         {testimonials.map((testimonial, index) => (
           <div
             key={testimonial.id}
@@ -39,9 +39,9 @@ const TestimonialSlider = () => {
             onClick={() => handleSlideChange(index)}
           />
         ))}
-      </div>
+      </div> */}
     </div>
   );
-}
+};
 
 export default TestimonialSlider;
